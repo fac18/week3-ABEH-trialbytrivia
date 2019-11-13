43911f6ee8183967454d02a7208df5da130999d2
Ayub Mohamud <a.mohamud1@hotmail.com>
2019-11-13 17:25:19 +0000

    made generator work
    Co-authored-by: Bethany Scott <bethanyioscott@hotmail.co.uk>
    Co-authored-by: Gillian Yeomans <gillian@yeomans.org.uk>
    Co-authored-by: Maria Olanipekun <m.olanipekun@hotmail.com>

--- dom.js
+++ dom.js
@@ -1,18 +1,45 @@
-   ( function () {
+let storageOutput;
+
+   function createIncrementer () {
+       let a = 1;
+
+    return function addOneTo () {
+        return a++;
+    }
+   }
+
+   const questionGenerator = createIncrementer();
+   
+   ( function populateQuestion () {
     let responseQuestions;
     let xhr = new XMLHttpRequest();
-    let url = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple"
+let url = "https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple"
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             responseQuestions = JSON.parse(xhr.responseText);
-            
-            const question = document.querySelector(".question");
-            question.textContent = responseQuestions.results[0].question;
+            storage(responseQuestions);
+            updateDom(responseQuestions);
+
         }
+       
     };
     xhr.open("GET", url, true);
     xhr.send();
 })();
 
+function storage(data){
+storageOutput = data
+}
 
-    
+
+
+function updateDom(responseQuestions){
+    const question = document.querySelector(".question");
+    question.textContent = responseQuestions.results[questionGenerator()].question;
+}
+const nextB = document.querySelector(".next")
+nextB.addEventListener('click', function(){
+    // console.log(storageOutput);
+updateDom(storageOutput);
+})
+
