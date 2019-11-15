7873b894547393a326991d8995b2ee33c21e72eb
Gillian <gillian@yeomans.org.uk>
2019-11-15 11:40:55 +0000

    add gif functionality
    Co-authored-by: Bethany Scott <bethanyioscott@hotmail.co.uk>
    Co-authored-by: Maria Olanipekun <m.olanipekun@hotmail.com>
    Co-authored-by: Ayub Mohamud <a.mohamud1@hotmail.com>

--- logic.js
+++ logic.js
@@ -1,31 +1,22 @@
-
-
 // incrementer for the questions
 
+function createIncrementer() {
+  let a = 1;
 
-
-function createIncrementer () {
-    let a = 1;
-
- return function addOneTo () {
-     return a++;
- }
+  return function addOneTo() {
+    return a++;
+  };
 }
-
-
 
 // random generator for answers
 
 function shuffleArray(arr) {
-    let newArr = arr;
-    console.log(newArr);
-    for (let i = newArr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-    }
+  let newArr = arr;
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
 }
-
-
-
-module.exports = {createIncrementer, shuffleArray}
- 
+if (typeof module !== "undefined") {
+  module.exports = { createIncrementer, shuffleArray };
+}
